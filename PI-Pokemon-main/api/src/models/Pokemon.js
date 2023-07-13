fbeb9184eec28980a4645d8d2fc1e524b918eb79
unknown <hornmacarena12@hotmail.com>
2023-07-13 20:16:20 -0300

    prueba

--- PI-Pokemon-main/api/src/models/Pokemon.js
+++ PI-Pokemon-main/api/src/models/Pokemon.js
@@ -3,10 +3,41 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
+  sequelize.define('Pokemon', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    nombre:{
       type: DataTypes.STRING,
       allowNull: false,
     },
+    imagen:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vida:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    ataque:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    defensa:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    velocidad:{
+      type: DataTypes.INTEGER
+    },
+    altura:{
+      type: DataTypes.FLOAT
+    },
+    peso:{
+      type: DataTypes.FLOAT
+    }
   });
 };
--- PI-Pokemon-main/api/src/routes/index.js
+++ PI-Pokemon-main/api/src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const axios = require ('axios');
+const { Pokemon, Type } = require('../db')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +9,60 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const getApiInfo = async () => {
+    const apiUrl = await axios.get('https://pokeapi.co/api/v2/pokemon');
+    const apiInfo = await apiUrl.data.map(el => {
+        return {
+            id: el.id,
+            nombre: el.nombre,
+            imagen: el.imagen,
+            vida: el.vida,
+            ataque: el.ataque,
+            defensa: el.defensa,
+            velocidad: el.velocidad,
+            altura: el.altura,
+            peso: el.peso
+            
+        };
+    });
+    return apiInfo;
+};
+
+const getDbInfo = async () => {
+    return await Pokemon.findAll({
+        include:{
+            model:Type,
+            attributes: ['name'],
+            through: {
+                attributes: [],
+            },
+        }
+    })
+}
+
+const getAllPokemon = async () => {
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    const infoTotal = apiInfo.concat(dbInfo);
+    return infoTotal
+}
+
+router.get('/', async (req, res) => {
+    const name = req.query.name
+    let allPokemons = await getAllPokemon();
+    if (name) {
+        let pokemonName = await allPokemons.filter(p => p.name.toLowerCase().includes(name.toLowerCase())) //compara los dos nombre en minusula para que no haya errores
+        pokemonName.length ?
+        res.status(200).send(pokemonName) :
+        res.status(404).send('No esta el personaje');
+    } else {
+        res.status(200).send(allPokemons)
+    }
+})
+
+router.get('/:idPokemon', async (req, res) => {
+    
+})
 
 
 module.exports = router;
